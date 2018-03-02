876b08f86d90ac7b60ad0b7665546226a90c2c08
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-02 08:28:51 -0500

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-example-pod-using-restkit_Tests/Pods-example-pod-using-restkit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_example_pod_using_restkit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_example_pod_using_restkit_Tests
+@end
---
+++ Example/Pods/Target Support Files/example-pod-using-restkit/example-pod-using-restkit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_example_pod_using_restkit : NSObject
+@end
+@implementation PodsDummy_example_pod_using_restkit
+@end
